e0a1eef488343826827cccd8fa7cf4b9252a4c8c
neverchanje <wutao.as.neverchanje@gmail.com>
2021-03-11 11:35:48 +0800

    fix lint

--- pegasus/client_test.go
+++ pegasus/client_test.go
@@ -7,7 +7,6 @@ package pegasus
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"sync"
 	"testing"
 	"time"
@@ -144,30 +143,4 @@ func TestPegasusClient_New(t *testing.T) {
 		MetaServers: []string{},
 	})
 	assert.NotNil(t, err)
-}
-
-//generate random bytes
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-func randomBytes(n int) []byte {
-	b := make([]byte, n)
-	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
-	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = rand.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return b
 }
